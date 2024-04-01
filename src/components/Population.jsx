3bd0a371f3ae38898ac04379514a404701d21581
Jatin-Rai <jatinrai258@gmail.com>
2024-04-01 12:21:04 +0530

    Added: Chart for population data

--- src/components/Population.jsx
+++ src/components/Population.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
 import useFetch from "../hooks/useFetch";
+import { defaults } from 'chart.js/auto';
 import { Line } from 'react-chartjs-2';
 import Spinner from "./Spinner";
+
+defaults.color = 'white';
+
+const chartOptions = {
+    scales: {
+        x: {
+            ticks: {
+                color: 'white', // X-axis tick color
+            },
+            grid: {
+                color: 'rgba(255, 255, 255, 0.1)', // X-axis grid color
+            },
+        },
+        y: {
+            ticks: {
+                color: 'white', // Y-axis tick color
+            },
+            grid: {
+                color: 'rgba(255, 255, 255, 0.1)', // Y-axis grid color
+            },
+        },
+    },
+    plugins: {
+        legend: {
+            labels: {
+                color: 'white', // Legend label color
+            },
+        },
+    },
+};
 
 const Population = () => {
     const { data, loading, error } = useFetch("https://datausa.io/api/data?drilldowns=Nation&measures=Population");
@@ -9,36 +39,38 @@ const Population = () => {
     if (loading) return <Spinner />;
     if (error) return <p>Error: {error.message}</p>;
 
-    const chartData = {
-        labels: data.map(item => item.Year),
-        datasets: [
-            {
-                label: 'Population',
-                data: data.map(item => parseInt(item.Population)),
-                fill: false,
-                borderColor: 'rgb(75, 192, 192)',
-            },
-        ],
-    };
+    const sortedData = data.data.sort((a, b) => a.Year - b.Year);
+    const nation = data.data[0].Nation;
+
+    const { annotations } = data.source[0]
 
     return (
-        <div className="mt-6">
-            <h2>Population Data</h2>
-            <div style={{ height: '400px', width: '600px' }}>
+        <div className="mx-3 mt-6">
+            <h2 className='text-3xl font-semibold mb-10'>{nation} Population Data</h2>
+            <div className='w-5/6 h-auto'>
                 <Line
-                    data={data}
-                    options={{
-                        plugins: {
-                            title: {
-                                display: true,
-                                text: "Population"
-                            },
-                            legend: {
-                                display: false
+                    data={{
+                        labels: sortedData.map(item => item.Year),
+                        datasets: [
+                            {
+                                label: "Population(in Millions)",
+                                data: sortedData.map(item => item.Population / 1000000),
+                                borderColor: 'lightgreen',
+                                backgroundColor: 'white',
+                                tension: 0.1
                             }
-                        }
+                        ]
                     }}
+                    options={chartOptions}
                 />
+                <div className='mt-6 text-center text-sm'>
+                    <p>{annotations.source_name} - {annotations.dataset_name}</p>
+                    <p className='text-xs italic mt-4'>
+                        {annotations.source_description} Refer
+                        <a href={annotations.dataset_link} target="_blank" rel="noopener noreferrer"> {annotations.dataset_link} </a>
+                        for more details
+                    </p>
+                </div>
             </div>
         </div>
     );
